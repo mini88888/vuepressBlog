afa31dbd121d83415664ce51725952e6e0035649
v_qinqyu <v_qinqyu@tencent.com>
2023-09-25 16:37:15 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com/mini88888/vuepressBlog.git masterr:gh-pages
+git push -f git@github.com/mini88888/vuepressBlog.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
